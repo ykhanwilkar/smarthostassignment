e545bd4fb59dfb563b63152ac5899366293c4265
Yash Khanwilkar <KhanwilkarYash@JohnDeere.com>
2026-02-09 15:45:56 +0530

    Updating the run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-# This file will be included as a Docker ENTRYPOINT in our automated testing evironment.
-gradle clean test build bootRun
+gradle clean test build
+gradle bootRun
