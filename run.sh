bdbdc9ec58a3b41adeb96da6b825bacf3c0a969e
yash <ykhanwilkar@gmail.com>
2026-02-19 19:39:08 +0530

    updating the run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-sudo chmod -R 777 .gradle/
-gradle clean test build
-java -jar ./build/libs/SmartRoomAllocator-0.0.1-SNAPSHOT.jar
+#sudo chmod -R 777 .gradle/
+gradle clean bootRun
+#java -jar ./build/libs/SmartRoomAllocator-0.0.1-SNAPSHOT.jar
